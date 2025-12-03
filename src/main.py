8986e76251c9faacc4c7cd7b2392367e0901256a
Yerra Kumara Venkata Kartikeya <venkatakartikeya@qaid.co.in>
2025-12-03 15:11:21 +0530

    codebase updated

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api.v1 import compliance, attendance
 
 app = FastAPI(title="HRMS Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(compliance.router, prefix="/api/v1")
 app.include_router(attendance.router, prefix="/api/v1/attendance_tracking", tags=["Attendance Tracking"])
